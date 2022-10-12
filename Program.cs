4d9f039207647d5426fc9c139aa3a6d168f1a1f2
ベンジー <benjysugi01@outlook.com>
2022-10-12 12:25:48 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace InternetArchiveDownloader {
+    internal class Program {
+        static string temp = Environment.GetEnvironmentVariable("TEMP");
+
+        static void Main(string[] args) {
+            Console.Write("Enter the ID (the bit after the /details/:> ");
+            string id = Console.ReadLine();
+
+            Console.Write("Enter the path you want to download to :> ");
+            string dlFinal = Console.ReadLine();
+
+            string downloadRoot = "https://archive.org/download";
+
+            string xmlLocalPath = temp + "\\" + id + ".xml";
+
+            try {
+                string dlPath = $"{downloadRoot}/{id}/{id}_files.xml";
+
+                using (var wc = new WebClient()) {
+                    wc.DownloadFile(dlPath, xmlLocalPath);
+                }
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+
+            if(File.Exists(xmlLocalPath)) {
+                XmlSerializer serializer = new XmlSerializer(typeof(XmlSchema.Files));
+
+                XmlSchema.Files deserialized;
+
+                using (Stream reader = new FileStream(xmlLocalPath, FileMode.Open)) {
+                    deserialized = (XmlSchema.Files)serializer.Deserialize(reader);
+                }
+
+                File.Delete(xmlLocalPath);
+
+                foreach (var file in deserialized.File) {
+                    Console.WriteLine(file.Name);
+
+                    string fileName = file.Name;
+                    string fileNamePathFriendly = file.Name.Replace("/", "\\");
+
+                    string dlPath = dlFinal + "\\" + fileNamePathFriendly;
+
+                    string webFile = $"{downloadRoot}/{id}/{fileName}";
+
+                    try {
+                        Directory.CreateDirectory(Path.GetDirectoryName(dlPath));
+                    }
+                    catch(Exception ex) {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    try {
+                        using(var wc = new WebClient()) {
+                            wc.DownloadFile(webFile, dlPath);
+                        }
+                    }
+                    catch(Exception ex) {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Schema.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+namespace XmlSchema {
+    [XmlRoot(ElementName = "file")]
+    public class File {
+        [XmlElement(ElementName = "mtime")]
+        public string Mtime { get; set; }
+        [XmlElement(ElementName = "size")]
+        public string Size { get; set; }
+        [XmlElement(ElementName = "md5")]
+        public string Md5 { get; set; }
+        [XmlElement(ElementName = "crc32")]
+        public string Crc32 { get; set; }
+        [XmlElement(ElementName = "sha1")]
+        public string Sha1 { get; set; }
+        [XmlElement(ElementName = "format")]
+        public string Format { get; set; }
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+        [XmlAttribute(AttributeName = "source")]
+        public string Source { get; set; }
+        [XmlElement(ElementName = "rotation")]
+        public string Rotation { get; set; }
+        [XmlElement(ElementName = "length")]
+        public string Length { get; set; }
+        [XmlElement(ElementName = "height")]
+        public string Height { get; set; }
+        [XmlElement(ElementName = "width")]
+        public string Width { get; set; }
+        [XmlElement(ElementName = "btih")]
+        public string Btih { get; set; }
+        [XmlElement(ElementName = "summation")]
+        public string Summation { get; set; }
+    }
+
+    [XmlRoot(ElementName = "files")]
+    public class Files {
+        [XmlElement(ElementName = "file")]
+        public List<File> File { get; set; }
+    }
+
+}
